b1914e9b57a0c7ca33d0f3e00d6effd6581df5a7
João Inácio <jgci2000@hotmail.com>
2022-02-04 09:34:48 +0100

    changed some simulation parameters and script is now working as intended

--- project_1/velocity_distrib.py
+++ project_1/velocity_distrib.py
@@ -14,9 +14,9 @@ with open(file_name, "r") as file:
         file.readline()
     
     N = int(file.readline().strip())
-    v_x = np.zeros((N, len(frames) + 1))
-    v_y = np.zeros((N, len(frames) + 1))
-    v_z = np.zeros((N, len(frames) + 1))
+    v_x = np.zeros((N, len(frames)))
+    v_y = np.zeros((N, len(frames)))
+    v_z = np.zeros((N, len(frames)))
     
     for _ in range(5):
         file.readline()
@@ -42,39 +42,83 @@ with open(file_name, "r") as file:
                     v_z[i, count] = float(line[7])
         except:
             continue
-    
-filenames = list()
 
-for i in range(len(frames) + 1):
+# T = 300
+# def maxwell_velocity(v, T):
+#     return 4 * np.pi * (1 / (2 * np.pi * T))**(3/2) * v**2 * np.exp(- v**2 / (2 * T))
+
+filenames = list()
+for i in range(len(frames)):
     plt.figure("Distribution of Velocities")
     n_bins = 30
 
-    # vx
-    plt.subplot(3, 1, 1)
-    plt.hist(v_x[:, i], bins=n_bins, label="init")
-    plt.xlabel(r"$v_x$")
-    plt.ylabel(r"$P(v_x)$")
-
-    # vy
-    plt.subplot(3, 1, 2)
-    plt.hist(v_y[:, i], bins=n_bins, label="init")
-    plt.xlabel(r"$v_y$")
-    plt.ylabel(r"$P(v_y)$")
-
-    # vz
-    plt.subplot(3, 1, 3)
-    plt.hist(v_z[:, i], bins=n_bins, label="init")
-    plt.xlabel(r"$v_z$")
-    plt.ylabel(r"$P(v_z)$")
+    # v
+    plt.hist(np.sqrt(v_x[:, i]**2 + v_y[:, i]**2 + v_z[:, i]**2), density=True, bins=n_bins)
+    # plt.plot(np.linspace(0, 6, 1000), maxwell_velocity(np.linspace(0, 6, 1000), T), '-r', label="Maxwell-Boltzmann Distribution")
+    plt.xlabel(r"$v$")
+    plt.ylabel(r"$P(v)$")
+    plt.xlim(0, 6)
+    plt.ylim(0, 0.8)
+    # plt.legend()
 
     # create file name and append it to a list
     filename = f'{i}.png'
     filenames.append(filename)
     plt.savefig(filename)
     plt.close()
+    
+    print(f"{i+1}/{len(frames)}", end="\r")
 
 # build gif
-with imageio.get_writer('mygif.gif', mode='I') as writer:
+with imageio.get_writer('velocity.gif', mode='I') as writer:
+    for filename in filenames:
+        image = imageio.imread(filename)
+        writer.append_data(image)
+        
+# Remove files
+for filename in set(filenames):
+    os.remove(filename)
+
+
+filenames = list()
+for i in range(len(frames)):
+    plt.figure("Distribution of Velocities", figsize=(6, 10))
+    n_bins = 30
+
+    # vx
+    plt.subplot(3, 1, 1)
+    plt.hist(v_x[:, i], density=True, bins=n_bins)
+    plt.xlabel(r"$v_x$")
+    plt.ylabel(r"$P(v_x)$")
+    plt.xlim(-5, 5)
+    plt.ylim(0, 0.3)
+
+    # vy
+    plt.subplot(3, 1, 2)
+    plt.hist(v_y[:, i], density=True, bins=n_bins)
+    plt.xlabel(r"$v_y$")
+    plt.ylabel(r"$P(v_y)$")
+    plt.xlim(-5, 5)
+    plt.ylim(0, 0.3)
+
+    # vz
+    plt.subplot(3, 1, 3)
+    plt.hist(v_z[:, i], density=True, bins=n_bins)
+    plt.xlabel(r"$v_z$")
+    plt.ylabel(r"$P(v_z)$")
+    plt.xlim(-5, 5)
+    plt.ylim(0, 0.3)
+
+    # create file name and append it to a list
+    filename = f'{i}.png'
+    filenames.append(filename)
+    plt.savefig(filename)
+    plt.close()
+    
+    print(f"{i+1}/{len(frames)}", end="\r")
+
+# build gif
+with imageio.get_writer('velocity_component.gif', mode='I') as writer:
     for filename in filenames:
         image = imageio.imread(filename)
         writer.append_data(image)
